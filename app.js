7b4535c5314f98af0917bb59c58ad61bf6572e71
akashgadiparthi <143130459+akashgadiparthi@users.noreply.github.com>
2023-11-01 01:09:47 -0500

    add

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var FoxRouter = require('./routes/Fox');
+var boardRouter = require('./routes/board');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Fox', FoxRouter);
+app.use('/board', boardRouter);
 
 
 // catch 404 and forward to error handler
