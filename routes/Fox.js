41a24ae8f867eca93ee32a1ab9e9d8e81d32b597
akashgadiparthi <143130459+akashgadiparthi@users.noreply.github.com>
2023-11-01 00:51:24 -0500

    Update Fox.js

--- routes/Fox.js
+++ routes/Fox.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Deer', { title: 'Search Results Fox' });
+  res.render('Fox', { title: 'Search Results Fox' });
 });
 module.exports = router;
